9c1b096d9e4e58eb7a86e25f84c820ae80bd797a
myoponeponechit <myoponeponechit@gmail.com>
2022-08-18 13:26:46 +0630

    06 - javaFX

---
+++ 06-javafxlesson/src/com/mmit/StartController.java
@@ -0,0 +1,50 @@
+package com.mmit;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class StartController {
+	 @FXML
+	    private TextField txtEmail;
+
+	    @FXML
+	    private PasswordField txtPassword;
+
+	    @FXML
+	    void btnClear_click(ActionEvent event) {
+	    	txtEmail.setText("");
+	    	txtPassword.setText(null);
+	    }
+
+	    @FXML
+	    void btnLogin_click(ActionEvent event) {
+	    	String email = txtEmail.getText();
+	    	String pass = txtPassword.getText();
+	    	
+	    	if(email.equals("admin@gmail.com") && pass.equals("admin")) {
+	    		Alert alert = new Alert(AlertType.INFORMATION);
+	    		alert.setContentText("Login success");
+	    		alert.setHeaderText("Message");
+	    		alert.show();
+	    	}
+	    	else {
+	    		Alert alert = new Alert(AlertType.ERROR);
+	    		alert.setContentText("Incorrect email and password");
+	    		alert.setHeaderText("Error Message");
+	    		alert.show();
+	    	}
+	    }
+	    
+
+	    @FXML
+	    void lblRegister_click(MouseEvent event) throws IOException {
+	    	Main.changeScene("Register.fxml");
+	    }
+}
